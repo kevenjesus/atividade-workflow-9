113c8225a5c5a306b9ed0e94496e771ed68a135d
Clebson Pettenon <clebsonpettenon@MacBook-Air-de-Clebson.local>
2017-01-25 15:37:07 -0300

    Atividade Workflow

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify-css', function() {
+  return gulp.src('source/scss/*.scss')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('source/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('monitorar', function(){
+        gulp.watch('source/scss/*.scss',['minify-css']);
+        gulp.watch('source/index.html',['minify-html']);
+});
